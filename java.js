38d38d27c8b4c7557b54329021dc6f8aec4cd92c
Andrzej Markiel <a.markiel@softkraft.co>
2022-11-17 10:18:36 +0100

    .

---
+++ java.js
@@ -0,0 +1,7 @@
+const input = document.getElementsByClassName("input");
+const history = document.getElementsByClassName("history");
+const numberButtons = document.querySelectorAll("number");
+const deleteButton = document.getElementsByClassName("delete");
+const clearButton = document.getElementsByClassName("clear");
+const dotButton = document.getElementsByClassName("dot");
+const equalsButton = document.getElementsByClassName("equal");
